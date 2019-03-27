f132ae16319e7cfd3df5aaad46dd9858c20b8255
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-03-27 12:54:42 -0700

    Completed displaying dummy students data

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,33 @@
+
+export default [
+    {
+        name: 'Carl Junior',
+        course: 'History',
+        grade: 93,
+        id: 1
+    },
+    {
+        name: 'Chuckie Cheese',
+        course: 'Architecture',
+        grade: 51,
+        id: 2
+    },
+    {
+        name: 'Ronald McDonald',
+        course: 'Geometry',
+        grade: 80,
+        id: 3
+    },
+    {
+        name: 'Burger King',
+        course: 'English',
+        grade: 15,
+        id: 4
+    },
+    {
+        name: 'Colonel Sanders',
+        course: 'Chemistry',
+        grade: 38,
+        id: 5
+    }
+];
