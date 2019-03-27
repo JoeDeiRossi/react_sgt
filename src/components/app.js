08d565077cd4deb875007e83a5e1d0c106b64f43
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-03-27 16:45:55 -0700

    Completed add student with dummy data

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,50 @@
 
+import 'materialize-css/dist/js/materialize.min';
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
 import '../assets/css/app.scss';
+import React, {Component} from 'react';
 import StudentsTable from './students_table';
+import AddStudent from './add_student';
+import studentData from '../dummy_data/student_list';
 
-const App = () => (
-    <div>
-        <h1 className="center">React SGT</h1>
+let id = 100;
 
-        <div className="row">
-            <StudentsTable/>
-        </div>
-    </div>
-);
+class App extends Component {
+    state = {
+        students: []
+    }
+
+    addStudent = (student) => {
+        student.id = id++;
+        this.setState({
+            students: [...this.state.students, student]
+        });
+    }
+
+    componentDidMount() {
+        this.getStudentData();
+    }
+
+    getStudentData() {
+        //Call server here
+
+        this.setState({
+            students: studentData
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 className="center">React SGT</h1>
+
+                <div className="row">
+                    <StudentsTable col="s12 m8" list={this.state.students}/>
+                    <AddStudent col="s12 m4" add={this.addStudent}/>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
